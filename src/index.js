4e3435c005b69bc33ef80e010d2eac3e2d7df6c2
nasirisara <99367926+nasirisara@users.noreply.github.com>
2022-06-12 21:37:37 +0200

    change

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ root.render(
   </React.StrictMode>
 );
 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+       <h1>Welcome to Workshop2</h1> 
+       <h3> Sara</h3>    
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
