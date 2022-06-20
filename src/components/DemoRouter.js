0069fda1e49f611a0f69e37e042e7ac06eb1af68
nasirisara <99367926+nasirisara@users.noreply.github.com>
2022-06-20 14:19:07 +0200

    part1

---
+++ src/components/DemoRouter.js
@@ -0,0 +1,57 @@
+
+import { Route, Routes, Link } from "react-router-dom";
+
+
+
+const DemoRouter = () => {
+
+    return (
+        <>
+        <Header/>
+        <Routes>
+        <Route exact path="/" element={<Welcome/>} />
+            <Route path="/home" element={<Home/>} />
+            <Route path="/person" element={<Person/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/NotFound" element={<NotFound/>} />
+        </Routes>
+        </>
+    );
+
+};
+
+
+
+const Header = () => {
+return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container-fluid">
+            <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/home">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/">Welcome</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/person">Person</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="navbar-brand" to="/about">About Us</Link>
+                </li>
+            </ul>
+            <form>
+                <Link className="btn btn-dark" to="/person">Sign Up</Link>
+            </form>
+        </div>
+
+    </nav>
+    )
+};
+
+const Welcome = () => <b><h3>Welcome Page</h3></b>
+const Home = () => <b><h3>Home Page</h3></b>;
+const About = () => <b><h3>About Us Page</h3></b>;
+const Person = () => <b><h3>Person Page</h3></b>;
+const NotFound = () =><b><h3>Page Not Found</h3></b>;
+export default DemoRouter;
--- src/index.js
+++ src/index.js
@@ -12,21 +12,4 @@ root.render(
 );
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-       <h1>Welcome to Workshop2</h1> 
-       <h3> Sara</h3>    
-
-      </header>
-    </div>
-  );
-}
-
-export default App;
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
